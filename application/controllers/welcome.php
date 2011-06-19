c34702206d16fdd79a5ac8e03af6c2a87439db32
Shawn Dellysse <sdellysse@gmail.com>
2011-06-19 01:45:55 -0400

    b30

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,7 +2,9 @@
 
 class welcome extends CI_Controller {
     function index () {
+        $this->load->library('session');
         echo 'welcome/index';
+        var_dump($this->session->userdata('key'));
     }
 
 }
